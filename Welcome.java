8fff66bc3826c1522d69a3b4eef9c907331aa74f
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-06-01 14:28:13 +0200

    completado ejercicio 1 y 2 en Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,10 +1,27 @@
 //Inicializa la clase Welcome
 //Crea el método main
 
-//Ejercicio 1: Define el método greeting que imprima en terminal ¡Hola FemCoder!
-//Ejecuta el método greeting 
+public class Welcome {
+    
+    // --- BLOQUE DE EJECUCIÓN (main) ---
+    public static void main(String[] args) {
+        //Ejercicio 1: Ejecuta el método greeting 
+        mainGreeting();
 
-//Ejercicio 2: Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
-//Ejecuta el método greetingCoder 
+        //Ejecuta el método greetingCoder 
+        greetingCoder("Siuzanna");
+    }
 
-//¿Sabes donde definir el método y en dónde lo has de ejecutar? Si no lo sabes vuelve al archivo de Resources.md
+    // --- BLOQUE EJERCICIO 1 ---
+    //Ejercicio 1: Define el método greeting que imprima en terminal ¡Hola FemCoder!
+    public static void mainGreeting() {
+        System.out.println("¡Hola FemCoder!");
+    }
+
+    // --- BLOQUE EJERCICIO 2 ---
+    //Ejercicio 2: Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
+    public static void greetingCoder(String name) {
+        System.out.println("¡Hola " + name + "!");
+    }
+
+} // Cierre de la clase Welcome
